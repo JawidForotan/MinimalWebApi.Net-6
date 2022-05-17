035dcf275173df174c9ed61599e3b27aa97dc116
Jawid <jawidforotan100@gmail.com>
2022-05-17 22:19:38 +0200

    Minimal web API in .Net 6 entity framework core and sql server

---
+++ AspNetCoreMinimalWebApi/Program.cs
@@ -0,0 +1,97 @@
+﻿using AspNetWebApi.Data;
+using AspNetWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+// Register connection string from appsetting.json
+
+builder.Services.AddDbContext<DataContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("Connection")));
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+// Get all records from database
+
+async Task<List<Student>> GetAllStudents(DataContext context) =>
+    await context.students.ToListAsync();
+
+
+// Test get mehtod
+
+app.MapGet("/", () => "Hello from minimal web api in .Net 6");
+
+
+// GEt all students wihtout async await 😉
+
+app.MapGet("/getAll", (DataContext context) =>
+
+context.students.ToList());
+
+
+// Get student by id
+
+app.MapGet("/getOne/{id}", async (DataContext context, int id) =>
+await context.students.FindAsync(id) is (Student student) ? Results.Ok(student) : Results.NotFound("No, student found"));
+
+
+// Post to create new student
+
+app.MapPost("/creatNewStudent", async (DataContext context, Student newStudent) =>
+{
+    context.students.Add(newStudent);
+    await context.SaveChangesAsync();
+    return Results.Ok(await GetAllStudents(context));
+});
+
+
+// Update
+
+app.MapPut("/updateStudent/{id}", async (DataContext context, Student updateStudent, int id) =>
+{
+    var studentToUpdate = await context.students.FindAsync(id);
+    if(studentToUpdate == null) return Results.NotFound("No, student found");
+
+    studentToUpdate.Id = id;
+    studentToUpdate.Name = studentToUpdate.Name;
+    studentToUpdate.Age = updateStudent.Age;
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await GetAllStudents(context));
+});
+
+
+// Delete
+
+app.MapDelete("/student/{id}", async (DataContext context, int id) =>
+{
+    var studentToDelete = await context.students.FindAsync(id);
+    if (studentToDelete == null) return Results.NotFound("What the... it doesn't even exist in our database");
+    context.students.Remove(studentToDelete);
+    await context.SaveChangesAsync();
+
+    return Results.Ok(await GetAllStudents(context));
+});
+
+
+app.Run();
